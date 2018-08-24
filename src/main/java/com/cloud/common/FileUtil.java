c11389fb9dc0ad3d155815c89609db0a507df19c
liulijun <liulj@huidiancloud.com>
2018-08-24 16:35:06 +0800

    加入文件操作工具类

---
+++ src/main/java/com/cloud/common/FileUtil.java
@@ -0,0 +1,14 @@
+package com.cloud.common;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * 文件操作工具类
+ * 实现文件的创建、删除、复制、以及目录的创建、删除、复制等功能
+ * @author lijun
+ */
+public class FileUtil {
+    private static Logger logger = LoggerFactory.getLogger(FileUtil.class);
+
+}
