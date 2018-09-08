f1729cc35f09370866b97075e6121212043f4b58
liulijun <liulj@huidiancloud.com>
2018-09-08 11:17:22 +0800

    增加in和not in实体

---
+++ src/main/java/com/cloud/common/base/IntervalEntity.java
@@ -0,0 +1,34 @@
+package com.cloud.common.base;
+
+import java.io.Serializable;
+
+/**
+ * 用来生成sql中的in和not in操作的对象
+ */
+public class IntervalEntity implements Serializable {
+    /**
+     * field comment: 名称
+     */
+    private String name;
+
+    /**
+     * field comment: 在in或者not in中的值
+     */
+    private Object list;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Object getList() {
+        return list;
+    }
+
+    public void setList(Object list) {
+        this.list = list;
+    }
+}
