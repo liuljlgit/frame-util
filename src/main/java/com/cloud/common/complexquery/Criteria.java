f6e792a5be253e0600ee66e224660900f9de35ce
liulijun <liulj@huidiancloud.com>
2018-10-08 16:29:45 +0800

    增加代码生成工具的复杂查询

--- src/main/java/com/cloud/common/complexquery/Criteria.java
+++ src/main/java/com/cloud/common/complexquery/Criteria.java
@@ -101,4 +101,92 @@ public class Criteria {
     public void setOpt(String opt) {
         this.opt = opt;
     }
+
+
+    /*以下为common的sql语句*/
+    public void andColIsNull(String col){
+        addCriterion("and "+col+" is null");
+    }
+    public void orColIsNull(String col){
+        addCriterion("or "+col+" is null");
+    }
+    public void andColIsNotNull(String col){
+        addCriterion("and "+col+" is not null");
+    }
+    public void orColIsNotNull(String col){
+        addCriterion("or "+col+" is not null");
+    }
+    public void andColEqualTo(String col,Object val){
+        addCriterion("and "+col+" = ",val);
+    }
+    public void orColEqualTo(String col,Object val){
+        addCriterion("or "+col+" = ",val);
+    }
+    public void andColNotEqualTo(String col,Object val){
+        addCriterion("and "+col+" <> ",val);
+    }
+    public void orColNotEqualTo(String col,Object val){
+        addCriterion("or "+col+" <> ",val);
+    }
+    public void andColGreaterThan(String col,Object val){
+        addCriterion("and "+col+" > ",val);
+    }
+    public void orColGreaterThan(String col,Object val){
+        addCriterion("or "+col+" > ",val);
+    }
+    public void andColGreaterThanOrEqualTo(String col,Object val){
+        addCriterion("and "+col+" >= ",val);
+    }
+    public void orColGreaterThanOrEqualTo(String col,Object val){
+        addCriterion("or "+col+" >= ",val);
+    }
+    public void andColLessThan(String col,Object val){
+        addCriterion("and "+col+" < ",val);
+    }
+    public void orColLessThan(String col,Object val){
+        addCriterion("or "+col+" < ",val);
+    }
+    public void andColLessThanOrEqualTo(String col,Object val){
+        addCriterion("and "+col+" <= ",val);
+    }
+    public void orColLessThanOrEqualTo(String col,Object val){
+        addCriterion("or "+col+" <= ",val);
+    }
+    public void andColLike(String col,Object val){
+        addCriterion("and "+col+" like ",val);
+    }
+    public void orColLike(String col,Object val){
+        addCriterion("or "+col+" like ",val);
+    }
+    public void andColNotLike(String col,Object val){
+        addCriterion("and "+col+" not like ",val);
+    }
+    public void orColNotLike(String col,Object val){
+        addCriterion("or "+col+" not like ",val);
+    }
+    public void andColIn(String col,Object val){
+        addCriterion("and "+col+" in ",val);
+    }
+    public void orColIn(String col,Object val){
+        addCriterion("or "+col+" in ",val);
+    }
+    public void andColNotIn(String col,Object val){
+        addCriterion("and "+col+" not in ",val);
+    }
+    public void orColNotIn(String col,Object val){
+        addCriterion("or "+col+" not in ",val);
+    }
+    public void andColBetween(String col,Object val1,Object val2){
+        addCriterion("and "+col+" between ",val1,val2);
+    }
+    public void orColBetween(String col,Object val1,Object val2){
+        addCriterion("or "+col+" between ",val1,val2);
+    }
+    public void andColNotBetween(String col,Object val1,Object val2){
+        addCriterion("and "+col+" not between ",val1,val2);
+    }
+    public void orColNotBetween(String col,Object val1,Object val2){
+        addCriterion("or "+col+" not between ",val1,val2);
+    }
+
 }
