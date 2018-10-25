7ba3aa67b7a4df524844652d5a840dcf2ef58663
liulijun <liulj@huidiancloud.com>
2018-10-25 16:53:37 +0800

    增加集合处理工具类

---
+++ src/main/java/com/cloud/common/utils/CollectionUtil.java
@@ -0,0 +1,58 @@
+package com.cloud.common.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 集合处理工具类
+ */
+public class CollectionUtil {
+
+    /**
+     * 均分List列表
+     * @param source
+     * @param n
+     * @param <T>
+     * @return
+     */
+    public static <T> List<List<T>> averageAssign(List<T> source, int n) {
+        List<List<T>> result = new ArrayList<>();
+        int remaider = source.size() % n;  //(先计算出余数)
+        int number = source.size() / n;  //然后是商
+        int offset = 0;//偏移量
+        for (int i = 0; i < n; i++) {
+            List<T> value = null;
+            if (remaider > 0) {
+                value = source.subList(i * number + offset, (i + 1) * number + offset + 1);
+                remaider--;
+                offset++;
+            } else {
+                value = source.subList(i * number + offset, (i + 1) * number + offset);
+            }
+            result.add(value);
+        }
+        return result;
+    }
+
+    /**
+     * 按固定大小切分List列表
+     * @param list
+     * @param len
+     * @param <T>
+     * @return
+     */
+    public static <T> List<List<T>> splitList(List<T> list, int len) {
+        if (list == null || list.size() == 0 || len < 1) {
+            return null;
+        }
+        List<List<T>> result = new ArrayList<>();
+        int size = list.size();
+        int count = (size + len - 1) / len;
+        for (int i = 0; i < count; i++) {
+            List<T> subList = list.subList(i * len, ((i + 1) * len > size ? size : len * (i + 1)));
+            result.add(subList);
+        }
+        return result;
+    }
+
+}
