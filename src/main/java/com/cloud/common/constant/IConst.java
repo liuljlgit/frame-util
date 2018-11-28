d07c3467087d33450fd73b37ae045d4035ceeac6
liulijun <liulj@huidiancloud.com>
2018-11-28 18:08:51 +0800

    增加默认缓存常量

--- src/main/java/com/cloud/common/constant/IConst.java
+++ src/main/java/com/cloud/common/constant/IConst.java
@@ -15,7 +15,22 @@ public class IConst {
     public static final Integer DEFAULT_PAGE_SIZE = 1000 ;
 
     /**
-     * field comment: 默认redis缓存时间：15分钟
+     * field comment: 缓存15分钟
      */
-    public static final Long DEFAULT_EXPIRE_TIME = 60*15L ;
+    public static final Long MINUTE_15_EXPIRE = 900L ;
+
+    /**
+     * field comment: 缓存30分钟
+     */
+    public static final Long MINUTE_30_EXPIRE = 1800L ;
+
+    /**
+     * field comment: 缓存一个小时
+     */
+    public static final Long HOUR_1_EXPIRE = 3600L ;
+
+    /**
+     * field comment: 永不过期
+     */
+    public static final Long ZERO_EXPIRE = 0L ;
 }
