883f89e65a9c69b3bf826c48b34484c64906508b
liulijun <liulj@huidiancloud.com>
2018-12-13 18:11:26 +0800

    抛出异常修改

--- src/main/java/com/cloud/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/cloud/common/exception/GlobalExceptionHandler.java
@@ -18,20 +18,30 @@ public class GlobalExceptionHandler extends BaseController {
 
     private static final Logger logger = LoggerFactory.getLogger(GlobalExceptionHandler.class);
 
-    //系统异常
+    /**
+     * 业务异常
+     * @param request
+     * @param e
+     * @return
+     */
     @ExceptionHandler(BusiException.class)
     @ResponseBody
-    public String busiExcepitonHandler(HttpServletRequest request, Exception e) {
+    public String busiExcepitonHandler(HttpServletRequest request, BusiException e) {
         logger.error(e.getMessage(),e);
         return formatResponseParams(EXEC_ERROR,null,e.getMessage());
     }
 
-    //系统异常
+    /**
+     * 系统异常
+     * @param request
+     * @param e
+     * @return
+     */
     @ExceptionHandler(Exception.class)
     @ResponseBody
     public String defultExcepitonHandler(HttpServletRequest request, Exception e) {
         logger.error(e.getMessage(),e);
-        return formatResponseParams(EXEC_ERROR,null,e.getMessage());
+        return formatResponseParams(EXEC_ERROR,null,"系统繁忙,请稍后重试!");
     }
 
 }
